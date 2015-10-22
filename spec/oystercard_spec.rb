b4f717498ce467e3513b298d75e5277610ebfc30
thisdotrob <thisdotrob@gmail.com>
2015-10-22 11:41:59 +0100

    Add top_up functionality to Oystercard

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,11 +3,15 @@ require 'oystercard'
 describe Oystercard do
 
   context '#intialize' do
-
     it 'sets a balance of zero' do
       expect(subject.balance).to eq 0
     end
+  end
 
+  context '#top_up' do
+    it 'allows user to add balance' do
+      expect{subject.top_up(10)}.to change(subject, :balance).by(10)
+    end
   end
 
 end
